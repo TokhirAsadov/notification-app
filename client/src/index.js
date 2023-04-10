53ba473c648b64c6a95885ad1730a9a80dd3f3a2
Bismillah <guvalakat1603@gmail.con>
2023-04-10 17:58:39 +0500

    add dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,20 @@ import App from './App';
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
 
