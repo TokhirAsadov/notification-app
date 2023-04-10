aa5794935921782069a6db90b1cf3e041268a2ce
Bismillah <guvalakat1603@gmail.con>
2023-04-10 17:59:01 +0500

    add user page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
 import Login from "./pages/Login";
 import SuperAdmin from "./pages/SuperAdmin";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Login />}/>
         <Route exact path="/login" element={<Login />}/>
         <Route exact path="/super/*" element={<SuperAdmin />}/>
+        <Route exact path="/user/*" element={<UserPage />}/>
       </Routes>
     </Router>
    );
