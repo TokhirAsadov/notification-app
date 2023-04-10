dc4b1b654f9ba87c7a14022137d6bb3ab03ff394
Bismillah <guvalakat1603@gmail.con>
2023-04-10 18:01:12 +0500

    create repositories

---
+++ service/src/main/java/com/tohir/service/repository/PCommitRepository.java
@@ -0,0 +1,7 @@
+package com.tohir.service.repository;
+
+import com.tohir.service.entity.permissionPost.PCommit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PCommitRepository extends JpaRepository<PCommit,String> {
+}
---
+++ service/src/main/java/com/tohir/service/repository/PermissionPostRepository.java
@@ -0,0 +1,7 @@
+package com.tohir.service.repository;
+
+import com.tohir.service.entity.permissionPost.PermissionPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissionPostRepository extends JpaRepository<PermissionPost,String> {
+}
