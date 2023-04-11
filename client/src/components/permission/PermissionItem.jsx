1539a68d1ee2caac5390a9be0f778e16d32f883c
Bismillah <guvalakat1603@gmail.con>
2023-04-11 17:50:38 +0500

    add animations with framer-motion

--- client/src/components/permission/PermissionItem.jsx
+++ client/src/components/permission/PermissionItem.jsx
@@ -5,6 +5,7 @@ import {Input} from "antd";
 import axios from "axios";
 import {BASE_URL, getHeaders} from "../../utills/ServiceUrls";
 import {useSelector} from "react-redux";
+import { motion as m } from "framer-motion";
 
 const PermissionItem = ({permissionPostId,content,status,description,createdAt,updatedAt,createdBy,updatedBy,fromDate,toDate,commits}) => {
 
@@ -39,63 +40,82 @@ const PermissionItem = ({permissionPostId,content,status,description,createdAt,u
   }
 
   return (
-    <div key={permissionPostId} className={"container bg-white border p-4 rounded-lg transition"}>
-      <div className={"container flex justify-between"}>
+    <m.div layout key={permissionPostId} className={"container bg-white border p-4 rounded-lg"}>
+      <m.div layout className={"container flex justify-between"}>
         <div className={"container flex items-center justify-between"}>
           <div>{content}</div>
           <div className={"w-8 h-8 flex items-center justify-center hover:bg-gray-200 mr-8 cursor-pointer rounded-full transition"} onClick={()=> setOpenAllData(prevState => !prevState)}><MoreOutlined/></div>
         </div>
         <button className={`rounded p-2 text-gray-600 ${status ==='AT_PROCESS' ? 'bg-yellow-300' : status ==='SUCCESS' ? 'bg-green-500': 'bg-red-500'} ${role ? 'cursor-not-allowed':"cursor-pointer"}`}>{status}</button>
-      </div>
+      </m.div>
       {/*todo------------------ tuliq barcha ma`lumotlarni chiqarish kk --------------------*/}
       {
-        openAllData && <div className={"container flex flex-col gap-1 mt-4 rounded bg-gray-100 p-4 transition"}>
-            <div className={"container flex justify-between text-xs"}>
+        openAllData && <m.div className={"container flex flex-col gap-1 mt-4 rounded bg-gray-100 p-4 transition"}
+                              initial={{ opacity: 0 }}
+                              animate={{ opacity: 1 }}
+                              exit={{ opacity: 0 }}
+                              layout
+        >
+            <m.div layout className={"container flex justify-between text-xs"}>
               <div className={"flex align-middle justify-center gap-1"}><CalendarOutlined style={{ width:'16px',height:'16px', display:'flex', alignItems:'center',justifyContent:'center' }} /> {moment(new Date(fromDate)).format('HH:mm:ss DD-MM-YYYY')}</div>
               <ArrowRightOutlined style={{ width:'16px',height:'16px', display:'flex', alignItems:'center',justifyContent:'center' }} />
               <div className={"flex align-middle justify-center gap-1"}><CalendarOutlined style={{ width:'16px',height:'16px', display:'flex', alignItems:'center',justifyContent:'center' }} /> {moment(new Date(toDate)).format('HH:mm:ss DD-MM-YYYY')}</div>
-            </div>
-            <hr className={"w-full bg-white border-0 mt-1 "} style={{ height: '1px' }}/>
+            </m.div>
+            <m.hr layout className={"w-full bg-white border-0 mt-1 "} style={{ height: '1px' }}/>
 
-            <div className={"text-justify"}>{description}</div>
-        </div>
+            <m.div layout className={"text-justify"}>{description}</m.div>
+        </m.div>
       }
-      {!openAllData && <hr className={"w-32"}/>}
+      {!openAllData && <m.hr layout className={"w-32"}/>}
 
-      <div className={"container flex items-center mt-4 "}>
+      <m.div layout className={"container flex items-center mt-4 "}>
         <div className={"w-8 h-8 flex items-center justify-center p-2 text-lg rounded-full hover:bg-gray-200 cursor-pointer transition"} onClick={() => setOpenCommits(prevState => !prevState)}>
           <CommentOutlined />
         </div>
         <div className={"w-4 h-4 flex items-center justify-center text-xs ml-1"}>{commits?.length > 0 ? commits?.length : 0}</div>
-      </div>
+      </m.div>
 
       {
-        openCommits && <div className={"container flex flex-col gap-1 rounded bg-gray-100 p-4 transition"}>
-          <div className={"container flex gap-2"}>
+        openCommits && <m.div initial={{ opacity: 0 }}
+                              animate={{ opacity: 1 }}
+                              exit={{ opacity: 0 }}
+                              layout
+                              className={"container flex flex-col gap-1 rounded bg-gray-100 p-4 transition"}
+          style={{
+            transition: "transform 0.5s, width 0.5s,height 0.5s",
+            transitionDelay: "1s, 0.5s, 0.5s",
+            transitionTimingFunction: "cubic-bezier(0.075, 0.82, 0.165,1)"
+          }}
+        >
+          <m.div layout className={"container flex gap-2"}>
             <div className={"flex items-center justify-center text-xs"}>Commit:</div>
             <Input name={"commit"} value={commitObj?.commit} placeholder="Commit..." onChange={(e) => setCommitObj(prevState => ({...prevState,commit: e.target.value}))}/>
             <button onClick={() => submitCommit(commitObj)} className={"p-2 flex items-center justify-center rounded text-blue-500 border border-blue-500 hover:bg-blue-500 hover:text-white transition text-xs"}><SendOutlined /></button>
-          </div>
-          {openCommits  && commits?.length > 0  && <hr className={"w-full bg-white border-0 mt-1"} style={{ height: '1px' }}/>}
+          </m.div>
+          {openCommits  && commits?.length > 0  && <m.hr layout className={"w-full bg-white border-0 mt-1"} style={{ height: '1px' }}/>}
           {
-            commits?.map((commit,index) => <div key={index} className={"container flex italic items-center justify-between"}>
+            commits?.sort(function(o1,o2){
+              if (o1?.createdAt>o2?.createdAt)    return 1;
+              else if(o1?.createdAt<o2?.createdAt) return  -1;
+              else  return  0;
+            })?.map((commit,index) => <m.div layout key={index} className={"container flex italic items-center justify-between"}>
               <div>
                 {user?.id===commit?.createdBy ? "you: " : role ? "admin: " : "user: "}{commit?.content}
               </div>
               <button onClick={()=>deleteCommit(permissionPostId,commit?.id)} className={"w-8 h-8 flex items-center justify-center p-1 rounded-full bg-white text-red-500 hover:bg-red-500 hover:text-white transition "}><DeleteOutlined /></button>
-            </div>)
+            </m.div>)
           }
-        </div>
+        </m.div>
       }
 
-      <div className={"container flex items-center justify-end text-gray-500 italic mt-2"}>
+      <m.div layout className={"container flex items-center justify-end text-gray-500 italic mt-2"}>
         <div className={"flex items-center gap-1 text-xs"}>
           <CalendarOutlined /> {moment(new Date(createdAt)).format('HH:mm:ss DD-MM-YYYY')}
         </div>
         <div className={" ml-4"}>username</div>
-      </div>
+      </m.div>
 
-    </div>
+    </m.div>
   );
 };
 
