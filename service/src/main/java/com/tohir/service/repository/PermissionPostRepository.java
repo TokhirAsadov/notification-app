c0cf91688f39cf921bf0af23b399ea4aa5c75cdf
Bismillah <guvalakat1603@gmail.con>
2023-04-11 17:54:50 +0500

    add new function

--- service/src/main/java/com/tohir/service/repository/PermissionPostRepository.java
+++ service/src/main/java/com/tohir/service/repository/PermissionPostRepository.java
@@ -3,5 +3,9 @@ package com.tohir.service.repository;
 import com.tohir.service.entity.permissionPost.PermissionPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PermissionPostRepository extends JpaRepository<PermissionPost,String> {
+
+    List<PermissionPost> findAllByCreatedByOrderByCreatedAt(String createdBy);
 }
