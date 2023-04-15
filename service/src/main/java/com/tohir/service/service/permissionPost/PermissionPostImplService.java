ae266a00bc5715b9e57b41b5b040932c5edc2bca
Bismillah <guvalakat1603@gmail.con>
2023-04-15 15:20:54 +0500

    add functions

--- service/src/main/java/com/tohir/service/service/permissionPost/PermissionPostImplService.java
+++ service/src/main/java/com/tohir/service/service/permissionPost/PermissionPostImplService.java
@@ -1,14 +1,17 @@
 package com.tohir.service.service.permissionPost;
 
 import com.tohir.service.entity.User;
+import com.tohir.service.entity.enums.PPostStatus;
 import com.tohir.service.entity.permissionPost.PCommit;
+import com.tohir.service.entity.permissionPost.PNotification;
 import com.tohir.service.entity.permissionPost.PermissionPost;
+import com.tohir.service.payload.ChangeStatusDto;
 import com.tohir.service.payload.CommentRequest;
 import com.tohir.service.payload.PPermissionDto;
-import com.tohir.service.payload.UserDto;
 import com.tohir.service.repository.PCommitRepository;
 import com.tohir.service.repository.PermissionPostRepository;
 import com.tohir.service.repository.UserRepository;
+import com.tohir.service.service.notification.PNotificationService;
 import com.tohir.service.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.codec.ServerSentEvent;
@@ -19,6 +22,7 @@ import reactor.core.scheduler.Schedulers;
 import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,6 +32,7 @@ public class PermissionPostImplService implements PermissionPostService{
     private final PCommitRepository commitRepository;
     private final UserRepository userRepository;
     private final UserService userService;
+    private final PNotificationService notificationService;
 
 
 
@@ -38,16 +43,25 @@ public class PermissionPostImplService implements PermissionPostService{
 
     @Override
     public List<PPermissionDto> getAll() {
-        return postRepository.findAll().stream().map(this::generateDto).collect(Collectors.toList());
+        return postRepository.findAllByOrderByCreatedAtDesc().stream().map(this::generateDto).collect(Collectors.toList());
     }
 
     @Override
     public List<PPermissionDto> getAllById(String userId) {
-        return postRepository.findAllByCreatedByOrderByCreatedAt(userId).stream().map(this::generateDto).collect(Collectors.toList());
+        return postRepository.findAllByCreatedByOrderByCreatedAtDesc(userId).stream().map(this::generateDto).collect(Collectors.toList());
     }
 
     @Override
     public PermissionPost create(User user,PermissionPost post) {
+
+        User tohir = userRepository.getUserByLogin("tohir");
+        notificationService.save(PNotification.builder()
+                .delivered(false)
+                .content("new permission from " + user.getFirstName().charAt(0)+"."+user.getLastName())
+                .type(PPostStatus.COMMENT)
+                .userFrom(user)
+                .userTo(tohir).build());
+
         return postRepository.save(post);
     }
 
@@ -58,7 +72,30 @@ public class PermissionPostImplService implements PermissionPostService{
         commitRepository.save(commit);
         post.getCommits().add(commit);
 
+        if (Objects.equals(post.getCreatedBy(), user.getId())){
+            User tohir = userRepository.getUserByLogin("tohir");
+            notificationService.save(PNotification.builder()
+                    .delivered(false)
+                    .content("new comment from " + user.getFirstName().charAt(0)+"."+user.getLastName())
+                    .type(PPostStatus.COMMENT)
+                    .userFrom(user)
+                    .userTo(tohir).build());
+        }
+        else {
+            Optional<User> userOptional = userRepository.findById(post.getCreatedBy());
+            notificationService.save(PNotification.builder()
+                    .delivered(false)
+                    .content("new comment from " + user.getFirstName().charAt(0)+"."+user.getLastName())
+                    .type(PPostStatus.COMMENT)
+                    .userFrom(user)
+                    .userTo(userOptional.get()).build());
+        }
+
         //todo ------------------------   notification quwiw kk
+
+
+
+
 
         return postRepository.save(post);
     }
@@ -87,8 +124,24 @@ public class PermissionPostImplService implements PermissionPostService{
     }
 
     @Override
+    public PermissionPost changeStatusOfPost(User user, ChangeStatusDto dto) {
+        PermissionPost postByID = getPostByID(dto.getId());
+        postByID.setStatus(dto.getStatus());
+
+        Optional<User> userOptional = userRepository.findById(postByID.getCreatedBy());
+        notificationService.save(PNotification.builder()
+                .delivered(false)
+                .content(dto.getStatus().toString()+" new answer from " + user.getFirstName().charAt(0)+"."+user.getLastName())
+                .type(PPostStatus.COMMENT)
+                .userFrom(user)
+                .userTo(userOptional.get()).build());
+
+        return postRepository.save(postByID);
+    }
+
+    @Override
     public Flux<ServerSentEvent<List<PPermissionDto>>> streamPosts(String userId) {
-        return Flux.interval(Duration.ofSeconds(2))
+        return Flux.interval(Duration.ofSeconds(1))
                 .publishOn(Schedulers.boundedElastic())
                 .map(sequence -> ServerSentEvent.<List<PPermissionDto>>builder().id(String.valueOf(sequence))
                         .event("post-list-event").data(userId==null ? getAll() : getAllById(userId))
