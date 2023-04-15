06570926bc36430672d6f2761fbb62a74ad477b1
Bismillah <guvalakat1603@gmail.con>
2023-04-15 15:21:47 +0500

    add notification to redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,19 @@
 import {compose, configureStore} from "@reduxjs/toolkit";
 import user from './slice/user/user_slice'
+import notification from './slice/notification/notification_slice'
 import stringMiddleware from "./middleware/stringMiddleware";
 import {loadFromLocalStorage} from "./actions/user/user_actions";
+import {loadNotificationFromLocalStorage} from "./actions/notification/notification_action";
 
 const preloadedState = {
   user:{
     user: loadFromLocalStorage(),
     userId: "",
     userLoadingStatus: "start"
+  },
+  notification:{
+    notification: loadNotificationFromLocalStorage(),
+    notificationLoadingStatus: "start"
   },
 };
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -16,6 +22,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = configureStore({
   reducer: {
     user,
+    notification
   },
   middleware: (getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware)),
   devTools: process.env.NODE_ENV !== "production",
